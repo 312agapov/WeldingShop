e0e37c8aefec62307baacfc0f8ffe0f4c1233452
LEONID <312agapov@gmai.com>
2024-07-02 21:13:55 +0400

    Игрался с компонентами, понял, зачем используется Autowired

--- src/main/java/com/example/Leonid/WeldingShop/Entities/WeldingMachine.java
+++ src/main/java/com/example/Leonid/WeldingShop/Entities/WeldingMachine.java
@@ -3,13 +3,17 @@ package com.example.Leonid.WeldingShop.Entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @Table(name = "weldings")
+@Component
 public class WeldingMachine {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/example/Leonid/WeldingShop/WeldingShopApplication.java
+++ src/main/java/com/example/Leonid/WeldingShop/WeldingShopApplication.java
@@ -1,13 +1,19 @@
 package com.example.Leonid.WeldingShop;
 
+import com.example.Leonid.WeldingShop.Entities.WeldingMachine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class WeldingShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeldingShopApplication.class, args);
+		ApplicationContext context = SpringApplication.run(WeldingShopApplication.class, args);
+		WeldingMachine weld = context.getBean(WeldingMachine.class);
+		weld.setName("Сварог");
+		weld.setPrice(10000);
+		System.out.println(weld);
 	}
 
 }
